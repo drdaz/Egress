d31520ff0f08aba3385f3ac5e19b5be9fb2a8100
Darren Black <drdaz7@gmail.com>
2026-03-14 18:04:12 +0100

    Initial Commit

---
+++ VPNChecker/VPNCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VPNCheckerApp.swift
+//  VPNChecker
+//
+//  Created by Darren Black on 14/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct VPNCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VPNCheckerTests/VPNCheckerTests.swift
@@ -0,0 +1,16 @@
+//
+//  VPNCheckerTests.swift
+//  VPNCheckerTests
+//
+//  Created by Darren Black on 14/03/2026.
+//
+
+import Testing
+
+struct VPNCheckerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
